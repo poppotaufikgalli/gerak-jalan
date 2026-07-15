554fc1998ae8206dc9a2e29f503ba16ebb2c2ccb
Poppo <poppo.galli@gmail.com>
2026-07-15 11:40:09 +0700

    update 2026

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<!-- Google tag (gtag.js) -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-KK4YED9S43"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
+		function gtag() { dataLayer.push(arguments); }
 		gtag('js', new Date());
-		
+
 		gtag('config', 'G-KK4YED9S43');
 	</script>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Gerak Jalan Proklamasi Tahun 2025</title>
+	<title>Gerak Jalan Proklamasi Tahun 2026</title>
 	@vite(['resources/js/app.js', 'resources/scss/styles.scss'])
 </head>
+
 <body>
