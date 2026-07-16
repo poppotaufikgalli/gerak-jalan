6e88eea664c69630c1a13d1209168a1116994882
Poppo <poppo.galli@gmail.com>
2026-07-16 12:08:39 +0700

    update group

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,7 +6,7 @@
     <section id="hero">
       <div class="hero-container">
         <!-- <h1>Lomba Gerak Jalan Proklamasi<br>Tahun 2026</h1>
-                                                                      <a href="{{route('daftar-peserta')}}" class="btn-get-started scrollto">Daftar Peserta</a> -->
+                                                                        <a href="{{route('daftar-peserta')}}" class="btn-get-started scrollto">Daftar Peserta</a> -->
       </div>
     </section>
     <div class="position-absolute top-0 start-50 translate-middle-x">
@@ -86,7 +86,7 @@
                     href="https://wa.me/6281266097649" target="_blank">0812-6609-7649</a>; <a
                     href="https://wa.me/628122770400" target="_blank">0812-2770-400</a>)</li>
               </ul>
-              <!-- <img src="{{asset('/img/group-wh.jpeg')}}" class="img-fluid" alt="" style="width: 200px"> -->
+              <img src="{{asset('/img/group-wh-2026.jpeg')}}" class="img-fluid" alt="" style="width: 200px">
             </div>
             <p class="fw-bold">D. Nomor Peserta</p>
             <p>Nomor peserta akan diberikan pada:</p>
@@ -207,10 +207,10 @@
               <li><i class="ri-check-double-line"></i> Dilarang menggunakan pakaian dengan model menyerupai hantu</li>
             </ul>
             <!-- <a href="{{asset('REGULASI.GERAK.JALAN.2026.pdf')}}" target="_blank" class="btn-learn-more position-relative">
-                          Download Pengumuman <span
-                            class="position-absolute top-0 start-100 translate-middle badge rounded-pill text-bg-danger">Baru!!<span
-                              class="visually-hidden">Pengumuman Baru</span></span>
-                        </a> -->
+                            Download Pengumuman <span
+                              class="position-absolute top-0 start-100 translate-middle badge rounded-pill text-bg-danger">Baru!!<span
+                                class="visually-hidden">Pengumuman Baru</span></span>
+                          </a> -->
           </div>
         </div>
       </div>
@@ -293,17 +293,17 @@
             <img src="{{asset('/img/ppi.png')}}" class="img-fluid" alt="">
           </div>
           <!-- <div class="col d-flex align-items-center justify-content-center">
-                                                                                                                                                    <img src="{{asset('/img/ppi.png')}}" class="img-fluid" alt="">
-                                                                                                                                                  </div> -->
+                                                                                                                                                      <img src="{{asset('/img/ppi.png')}}" class="img-fluid" alt="">
+                                                                                                                                                    </div> -->
           <!-- <div class="col d-flex align-items-center justify-content-center">
-                                                                                                                                                    <img src="{{asset('/img/logo-igornas.png')}}" class="img-fluid" alt="">
-                                                                                                                                                  </div> -->
+                                                                                                                                                      <img src="{{asset('/img/logo-igornas.png')}}" class="img-fluid" alt="">
+                                                                                                                                                    </div> -->
           <!-- <div class="col d-flex align-items-center justify-content-center">
-                                                                                                                                                    <img src="{{asset('/img/koni.svg')}}" class="img-fluid" alt="">
-                                                                                                                                                  </div> -->
+                                                                                                                                                      <img src="{{asset('/img/koni.svg')}}" class="img-fluid" alt="">
+                                                                                                                                                    </div> -->
           <!-- <div class="col d-flex align-items-center justify-content-center">
-                                                                                                                                                    <img src="{{asset('/img/logo-hut-ri-79.png')}}" class="img-fluid" alt="">
-                                                                                                                                                  </div> -->
+                                                                                                                                                      <img src="{{asset('/img/logo-hut-ri-79.png')}}" class="img-fluid" alt="">
+                                                                                                                                                    </div> -->
         </div>
       </div>
     </section><!-- End Clients Section -->
